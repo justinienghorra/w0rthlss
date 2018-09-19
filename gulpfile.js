32125782bbb4f524c465790d4f663e9a59b65aff
Justinien Ghorra <justinienghorra@hotmail.fr>
2018-09-19 20:33:53 +0200

    v1.1.2 - Gulp cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp'),
-    rename = require('gulp-rename'),
     cleanCSS = require('gulp-clean-css'),
-    concatCss = require('gulp-concat-css'),
     ngAnnotate = require('gulp-ng-annotate'),
     uglify = require('gulp-uglify'),
     useref = require('gulp-useref'),
@@ -11,22 +9,7 @@ var paths = {
     htmlSource: 'index.html',
     htmlDist: 'dist/'
 }
-/*
-gulp.task('script', function () {
-	return gulp.src(paths.scriptSource)
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(rename('app.min.js'))
-		.pipe(gulp.dest(paths.scriptDest));
-});
 
-gulp.task('css', () => {
-    return gulp.src(paths.cssSources)
-        .pipe(concatCss('style.min.css'))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest(paths.cssDest));
-});
-*/
 gulp.task('all', function () {
     return gulp.src(paths.htmlSource)
         .pipe(useref())
